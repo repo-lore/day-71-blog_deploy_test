e2d526291660e29f1800a54a0d9f6c694b5aea0b
Lorenzo Pusello <lorenzo.pusello@gmail.com>
2024-04-07 03:48:21 +0200

    change app.config for DB --> os.env--> only DB_URI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-# from flask_gravatar import Gravatar
+#from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
